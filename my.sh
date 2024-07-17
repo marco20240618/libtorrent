99c98922a57502a28e1f28bcfd1c67269a21da3b
root <you@example.com>
2024-07-17 14:29:49 +0800

    xxx

--- my.sh
+++ my.sh
@@ -5,6 +5,7 @@ if [ -z "$1" ]; then
    echo "3 armeabi-v7a"
    echo "4 x86_64"
    echo "5 x86"
+   echo "6 windows-amd64"
    exit
 elif [ $1 -eq 1 ]; then
    target=linux-amd64
@@ -16,6 +17,8 @@ elif [ $1 -eq 4 ]; then
    target=x86_64
 elif [ $1 -eq 5 ]; then
    target=x86
+elif [ $1 -eq 6 ]; then
+   target=windows-amd64
 else
    echo "wrong target"
    exit
