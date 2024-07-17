e9006ad95e2e4e848f984015890a08a8104f56ff
root <you@example.com>
2024-07-17 11:55:03 +0800

    xxx

--- my.sh
+++ my.sh
@@ -1,5 +1,20 @@
+if [ -z "$1" ]; then
+   echo "please specify target"
+   echo "1 linux-amd64"
+   echo "2 arm64-v8a"
+   echo "3 armeabi-v7a"
+   exit
+elif [ $1 -eq 1 ]; then
+   target=linux-amd64
+elif [ $1 -eq 2 ]; then
+   target=arm64-v8a
+elif [ $1 -eq 3 ]; then
+   target=armeabi-v7a
+else
+  echo "wrong target" 
+  exit
+fi
+
 rm -rf build/
-#cmake -B build -Dlinux-amd64=on
-#cmake -B build -Darm64-v8a=on
-cmake -B build -Darmeabi-v7a=on
+cmake -B build -D$target=on
 cmake --build build -j $(nproc)
